1961a90863499b5f5acd7a11b5f846815f6dda1f
JordanNitta <Jordannittarodrigues@gmail.com>
2023-05-19 15:15:54 -0700

    practice

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -5,6 +5,10 @@ const MainHeader = () => {
     <div>
         <h1>SONNY waas here 5/19/23 2:37 PM</h1>
         <h1>Jordan was here</h1>
+        <div className='red-500'>
+          <p>loren idjfaklmsdkmsadmkmksdak</p>
+        </div>
+        
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, nulla tempore libero deserunt pariatur nemo exercitationem dolorem quod, hic consequatur rerum, esse nam at? Similique possimus harum a veniam quasi?</p>
         </div>
 
