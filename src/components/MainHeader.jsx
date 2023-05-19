8ef327d18967ae37fa0e25098baeb6c1b1a4c497
JordanNitta <Jordannittarodrigues@gmail.com>
2023-05-19 15:06:02 -0700

    Change added

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -4,14 +4,11 @@ const MainHeader = () => {
   return (
     <div>
         <h1>SONNY waas here 5/19/23 2:37 PM</h1>
-<<<<<<< HEAD
         <h1>Jordan was here</h1>
-=======
-        <h2>Added Another line</h2>
->>>>>>> e0e74ed1046b14cab35372c853035c282d18637d
-    </div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, nulla tempore libero deserunt pariatur nemo exercitationem dolorem quod, hic consequatur rerum, esse nam at? Similique possimus harum a veniam quasi?</p>
+        </div>
 
-  )
+        )
 }
 
 export default MainHeader
