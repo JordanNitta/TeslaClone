27a429dea98b47a3b102344c85c56057005a3404
chhuonso <103080113+chhuonso@users.noreply.github.com>
2023-05-19 15:08:07 -0700

    changes 5

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -5,6 +5,9 @@ const MainHeader = () => {
     <div>
         <h1>SONNY waas here 5/19/23 2:37 PM</h1>
         <h1>Jordan was here</h1>
+        <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, sed?</h3>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate tempora accusantium consequatur veniam vero animi.
+        </p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, nulla tempore libero deserunt pariatur nemo exercitationem dolorem quod, hic consequatur rerum, esse nam at? Similique possimus harum a veniam quasi?</p>
         </div>
 
