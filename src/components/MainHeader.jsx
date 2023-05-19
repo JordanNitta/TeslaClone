e0e74ed1046b14cab35372c853035c282d18637d
chhuonso <103080113+chhuonso@users.noreply.github.com>
2023-05-19 15:02:16 -0700

    added the changes

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -3,21 +3,10 @@ import React from 'react'
 const MainHeader = () => {
   return (
     <div>
-<<<<<<< HEAD
         <h1>SONNY waas here 5/19/23 2:37 PM</h1>
-    </div>
-=======
-
-
-        
         <h2>Added Another line</h2>
     </div>
 
-
-
-
-    
->>>>>>> SonnyFeatures
   )
 }
 
