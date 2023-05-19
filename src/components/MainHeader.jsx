429a19d856ca2f21577a83957cd23c9e9a1e9cf8
JordanNitta <Jordannittarodrigues@gmail.com>
2023-05-19 15:18:45 -0700

    first commit

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -3,16 +3,9 @@ import React from 'react'
 const MainHeader = () => {
   return (
     <div>
-        <h1>SONNY waas here 5/19/23 2:37 PM</h1>
-        <h1>Jordan was here</h1>
-        <div className='red-500'>
-          <p>loren idjfaklmsdkmsadmkmksdak</p>
-        </div>
         
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, nulla tempore libero deserunt pariatur nemo exercitationem dolorem quod, hic consequatur rerum, esse nam at? Similique possimus harum a veniam quasi?</p>
-        </div>
-
-        )
+    </div>
+  )
 }
 
 export default MainHeader
